3e92e3862763f18afd2cd9f9516a664805599e59
FredVenturin <frederico.venturin@gmail.com>
2026-03-03 16:03:01 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { ordersApi } from "./services/api";
 
-const API = "http://127.0.0.1:3000";
+const API = import.meta.env.VITE_API_URL;
 
 function App() {
   const [orders, setOrders] = useState([]);
